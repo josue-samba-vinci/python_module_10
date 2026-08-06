ec46afdfd2f59f226f1ab917d8bd2979f49a6d44
Josue Samba <josuesamba99@gmail.com>
2026-08-06 14:17:26 +0200

    functools partial

--- ex3/functools_artifacts.py
+++ ex3/functools_artifacts.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from functools import reduce, partial
 import operator
 from collections.abc import Callable
 from typing import Any
@@ -16,7 +16,14 @@ def spell_reducer(spells: list[int], operation: str) -> int:
         raise ValueError("Unknow operation")
 
 def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
-    ...
+    enchantment_dict: dict[str, Callable] = {}
+    enchantment_dict["Earth"] = partial(base_enchantment, power=50, element="Earth")
+    enchantment_dict["Air"] = partial(base_enchantment, power=50, element="Air")
+    enchantment_dict["Fire"] = partial(base_enchantment, power=50, element="Fire")
+    return enchantment_dict
+
+def base_enchantment(power: int, element: str, target: str) -> str:
+    return f"{element} element hits {target} with {power} power"
 
 def memoized_fibonacci(n: int) -> int:
     ...
@@ -33,3 +40,7 @@ if __name__ == "__main__":
         print(spell_reducer([1,2,3], "bla"))
     except ValueError as e:
         print(e)
+    enchantment = partial_enchanter(base_enchantment)
+    print(enchantment["Earth"](target="creature1"))
+    print(enchantment["Air"](target="creature2"))
+    print(enchantment["Fire"](target="creature3"))
